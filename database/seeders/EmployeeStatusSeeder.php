897aa81dda5ba4fe1258eacec68dfcc6ae711c99
German Boldyrev <boldyrev_german@mail.ru>
2025-03-09 22:27:50 +0300

    feat: employer model + statuses + status seeder

---
+++ database/seeders/EmployeeStatusSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EmployeeStatus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeeStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        EmployeeStatus::create(['name' => 'working']);
+        EmployeeStatus::create(['name' => 'on_vacation']);
+    }
+}
